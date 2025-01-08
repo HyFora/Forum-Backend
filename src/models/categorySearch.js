6a15b6ff5873f41e87f0ebfeac03519e469b1b3e
Yasin-Erden <y.erden@gmx.net>
2025-01-08 15:41:40 +0100

    Namensänderung

--- src/models/categorySearch.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const categorySearchSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String },
-});
-
-const Category = mongoose.model('Category', categorySearchSchema);
-export default Category;
