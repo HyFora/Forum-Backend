2e68311e176245b6af55e0489de6d09ae687fbe8
Yasin-Erden <y.erden@gmx.net>
2025-01-08 09:51:12 +0100

    passwordUtils hinzugefügt und fertig

---
+++ src/utils/passwordUtils.js
@@ -0,0 +1,16 @@
+import bcrypt from 'bcrypt';
+
+export const hashPassword = async (password) => {
+    const saltRounds = 10;
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    
+    return hashedPassword;
+    };
+
+
+export const comparePasswords = async (password, hashedPassword) => {
+    const isValid = await bcrypt.compare(password, hashedPassword);
+    
+    return isValid;
+    }
