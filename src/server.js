ed0862ab42a076107cd527b2eb3b03d7e2d9483b
Yasin-Erden <y.erden@gmx.net>
2025-01-09 09:57:42 +0100

    categoryRouter im Server eingefügt & dotenv ganz nach oben genommen

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
-import express from "express";
 import 'dotenv/config';
+
+import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
 import userRouter from "./routes/userRouter.js";
+import categoryRouter from "./routes/categoryRouter.js";
 import { middleware } from "./middlewares/middleware.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
@@ -18,6 +20,7 @@ app.use(cors());
 app.use(middleware.log);
 
 app.use("/user", userRouter);
+app.use("/category", categoryRouter);
 
 app.use("*", middleware.invalid);
 
