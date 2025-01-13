598dd7972596bd2d1a91b0f95c59b8b7fcd4f3cd
Yasin-Erden <y.erden@gmx.net>
2025-01-13 13:57:48 +0100

    threadsSearchModel user hinzugefügt

--- src/models/threadsSearchModel.js
+++ src/models/threadsSearchModel.js
@@ -3,9 +3,11 @@ import mongoose from 'mongoose';
 const threadSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
-    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+    likeCount: { type: Number, default: 0 },
     createdAt: { type: Date, default: Date.now },
-});
+    category: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  });
 
 const Thread = mongoose.model('Thread', threadSchema);
 export default Thread;
