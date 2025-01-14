27953c2b139aaae08d39abad726e46c1a6730036
Yasin-Erden <y.erden@gmx.net>
2025-01-14 11:38:36 +0100

    sort by Date variable auch angelegt, logik kommt

--- src/controllers/sortSearchController.js
+++ src/controllers/sortSearchController.js
@@ -3,3 +3,7 @@ import Thread from '../models/threadsSearchModel.js';
 export const sortThreadsByLikes = async (req, res) => {
     const { sort } = req.query;
 };
+
+export const sortThreadsByDate = async (req, res) => {
+    const { sort } = req.query;
+}  
