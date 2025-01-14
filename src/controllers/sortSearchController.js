b27d97721b6ef18777fcd622529745ca32587783
Yasin-Erden <y.erden@gmx.net>
2025-01-14 11:36:40 +0100

    sortierfunktion mit Like

--- src/controllers/sortSearchController.js
+++ src/controllers/sortSearchController.js
@@ -1,29 +1,5 @@
-import Thread from '../models/categorySearchModel.js';
+import Thread from '../models/threadsSearchModel.js';
 
-export const filterThreadsByCategory = async (req, res) => {
-    const { category } = req.params;
-
-    try {
-        const threads = await Thread.find({ category });
-        res.json(threads);
-    } catch (error) {
-        console.error('Fehler beim Filtern der Threads nach Kategorie:', error);
-        res.status(500).json({ error: 'Interner Serverfehler' });
-    }
-};
-
-export const filterThreadsByUser = async (req, res) => {
-    const { user } = req.query;
-
-    if (!user) {
-        return res.status(400).json({ error: 'Eine User-ID ist erforderlich' });
-    }
-
-    try {
-        const threads = await Thread.find({ user });
-        res.json(threads);
-    } catch (error) {
-        console.error('Fehler beim Filtern der Threads eines Nutzers:', error);
-        res.status(500).json({ error: 'Interner Serverfehler' });
-    }
+export const sortThreadsByLikes = async (req, res) => {
+    const { sort } = req.query;
 };
