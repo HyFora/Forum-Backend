a9522aa50c597d8d4fafd480b8fd13ec774d415b
Yasin-Erden <y.erden@gmx.net>
2025-01-14 11:40:32 +0100

    Route für die Suchfunktionen angelegt

--- src/routes/threadSearchRouter.js
+++ src/routes/threadSearchRouter.js
@@ -1,9 +1,29 @@
 import express from "express";
 
-import { searchThreads } from "../controllers/threadSearchController.js";
+import { searchThreads } from '../controllers/threadSearchController.js';
+import { searchCategories } from '../controllers/categorySearchController.js';
+import { filterThreadsByUser } from '../controllers/userSearchController.js';
+import { sortThreadsByLikes, sortThreadsByDate } from '../controllers/sortSearchController.js';
 
-const router = express.Router();
+export const router = express.Router();
 
-userRouter
-    .route("/")
+router
+    .route('/threads/search')
+    .get(searchThreads);
+
+router
+    .route('/threads/category/:category')
     .get(searchCategories);
+
+router
+    .route('/threads')
+    .get(filterThreadsByUser);
+
+router
+    .route('/threads/sort')
+    .get(sortThreadsByLikes);
+
+router
+    .route('/threads/sort/date')
+    .get(sortThreadsByDate);
+    
